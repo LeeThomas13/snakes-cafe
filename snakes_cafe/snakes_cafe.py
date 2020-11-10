7043ec3f9811a69f0dec919773d915df2f78c1ae
LeeThomas13 <lmtthomasritz@gmail.com>
2020-11-09 16:19:08 -0800

    added menu text, functionality still needs implementation

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,49 @@
+print("""
+**************************************
+
+**   Welcome to the Snakes Cafe!    **
+
+**   Please see our menu below.     **
+
+**
+
+** To quit at any time, type "quit" **
+
+Appetizers
+----------
+
+Wings 
+Cookies
+Spring Rolls
+
+Entrees
+-------
+
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***************************************
+**   What would you like to order?   **
+***************************************
+
+""")
+
+order = input('> ')
+
+orderString = f'one order of {order} has been added to your meal'
+
+print(orderString)
